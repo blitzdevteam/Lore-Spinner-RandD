b33d947d0e2369b83154726673620977b2358b45
Nima Asaadi <ni4.asadi.b@gmail.com>
2025-10-08 13:46:26 +0330

    Add: create Category filament resource; add category factory into DatabaseSeeder

---
+++ app/Filament/Manager/Resources/Categories/CategoryResource.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Filament\Manager\Resources\Categories;
+
+use App\Filament\Manager\Resources\Categories\Pages\ListCategories;
+use App\Filament\Manager\Resources\Categories\Schemas\CategoryForm;
+use App\Filament\Manager\Resources\Categories\Tables\CategoriesTable;
+use App\Models\Category;
+use BackedEnum;
+use Filament\Resources\Resource;
+use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
+use Filament\Tables\Table;
+use UnitEnum;
+
+final class CategoryResource extends Resource
+{
+    protected static ?string $model = Category::class;
+
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::Bars2;
+
+    protected static string|UnitEnum|null $navigationGroup = 'Entities';
+
+    public static function getNavigationBadge(): string
+    {
+        return (string) self::getEloquentQuery()->count();
+    }
+
+    public static function form(Schema $schema): Schema
+    {
+        return CategoryForm::configure($schema);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return CategoriesTable::configure($table);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => ListCategories::route('/'),
+        ];
+    }
+}
---
+++ app/Filament/Manager/Resources/Categories/Tables/CategoriesTable.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Filament\Manager\Resources\Categories\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+final class CategoriesTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('title')
+                    ->searchable(),
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable(),
+                TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                EditAction::make()
+                    ->modalWidth('md'),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
