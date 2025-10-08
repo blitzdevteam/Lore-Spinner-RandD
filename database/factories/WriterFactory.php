723ad5ccddf29ef258a7314550cc753d2a97e1e8
Nima Asaadi <ni4.asadi.b@gmail.com>
2025-10-08 12:27:48 +0330

    Add: create Writer model, migration, factory, and seeder with necessary fields

---
+++ database/factories/WriterFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Writer>
+ */
+class WriterFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'first_name' => fake()->firstName(),
+            'last_name' => fake()->lastName(),
+            'email' => fake()->unique()->safeEmail(),
+            'password' => 'password',
+            'username' => fake()->username(),
+            'bio' => fake()->paragraph(),
+            'is_active' => fake()->boolean(),
+            'email_verified_at' => fake()->dateTime(),
+            'last_active_at' => fake()->dateTime(),
+        ];
+    }
+}
+
--- database/migrations/2025_10_08_082249_create_writers_table.php
+++ database/migrations/2025_10_08_082249_create_writers_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('writers', function (Blueprint $table) {
             $table->id();
             $table->string('first_name');
             $table->string('last_name');
@@ -34,6 +34,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('writers');
     }
 };
