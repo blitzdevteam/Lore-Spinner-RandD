1ab6d8f1025b19aa84ba45787ecc3a9496807cf7
Nima Asaadi <ni4.asadi.b@gmail.com>
2025-10-09 16:24:26 +0330

    Add: implement StoryChapter model and migration, and establish relationship with Story

--- app/Models/Story.php
+++ app/Models/Story.php
@@ -7,6 +7,7 @@ use App\Enums\Story\StatusEnum;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 final class Story extends Model
 {
@@ -39,5 +40,13 @@ final class Story extends Model
     public function category(): BelongsTo
     {
         return $this->belongsTo(Category::class);
+    }
+
+    /**
+     * @return HasMany<$this, StoryChapter>
+     */
+    public function chapters(): HasMany
+    {
+        return $this->hasMany(StoryChapter::class);
     }
 }
---
+++ app/Models/StoryChapter.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class StoryChapter extends Model
+{
+    protected $guarded = [
+        'id', 'created_at', 'updated_at',
+    ];
+
+    /**
+     * @return BelongsTo<$this, Story>
+     */
+    public function story(): BelongsTo
+    {
+        return $this->belongsTo(Story::class);
+    }
+}
