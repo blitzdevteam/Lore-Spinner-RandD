1a5a359240aeb1f5fabcea33d9954425a6bba1ca
Sina Hatamian <sina.hatamian75@gmail.com>
2025-10-20 16:10:10 +0330

    Add: Footer and app main layout

--- resources/js/pages/home.tsx
+++ resources/js/pages/home.tsx
@@ -1,5 +1,5 @@
 import { Head } from '@inertiajs/react';
-import NavigationMenu from '@/components/ui/navigationMenu';
+import AppLayout from '@/components/layouts/AppLayout';
 
 export default function Home() {
     return (
@@ -8,8 +8,8 @@ export default function Home() {
                 <link rel="preconnect" href="https://fonts.bunny.net" />
                 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
             </Head>
-            <NavigationMenu />
-            <div className="flex min-h-screen flex-col items-center bg-[#FDFDFC] p-6 text-[#1b1b18] lg:justify-center lg:p-8 dark:bg-[#0a0a0a]">
+            <AppLayout>
+            <div className="flex min-h-screen flex-col items-center p-6 text-[#1b1b18] lg:justify-center lg:p-8 dark:text-[#EDEDEC]">
                 <div className="flex w-full items-center justify-center opacity-100 transition-opacity duration-750 lg:grow starting:opacity-0">
                     <main className="flex w-full max-w-[335px] flex-col-reverse lg:max-w-4xl lg:flex-row">
                         <div className="flex-1 rounded-br-lg rounded-bl-lg bg-white p-6 pb-12 text-[13px] leading-[20px] shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] lg:rounded-tl-lg lg:rounded-br-none lg:p-20 dark:bg-[#161615] dark:text-[#EDEDEC] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d]">
@@ -757,6 +757,7 @@ export default function Home() {
                     </main>
                 </div>
             </div>
+            </AppLayout>
         </>
     );
 }
