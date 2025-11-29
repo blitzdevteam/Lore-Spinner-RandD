a0eac8aee381a9bb5cd6e493e0b666e71a7a5aff
Nima Asaadi <ni4.asadi.b@gmail.com>
2025-11-29 12:47:33 +0330

    Add: implement user creation helper function and enhance AuthenticationTest with login functionality and assertions

--- tests/Feature/User/AuthenticationTest.php
+++ tests/Feature/User/AuthenticationTest.php
@@ -3,6 +3,11 @@
 use App\Models\User;
 use Inertia\Testing\AssertableInertia as Assert;
 
+function createUser($payload = []): User
+{
+    return User::factory()->create($payload);
+}
+
 describe('registration', function () {
     it('registration page can render', function () {
         $this
@@ -18,7 +23,7 @@ describe('registration', function () {
             'password_confirmation' => 'password',
         ];
 
-        User::factory()->create([
+        createUser([
             'email' => $payload['email'],
         ]);
 
@@ -46,5 +51,48 @@ describe('registration', function () {
             ->assertDatabaseHas('users', [
                 'email' => $payload['email']
             ]);
+    });
+});
+
+describe('login', function () {
+    it('login page can render', function () {
+        $this
+            ->get(route('user.authentication.login.create'))
+            ->assertOk()
+            ->assertInertia(fn(Assert $page) => $page->component('User/Authentication/Login'));
+    });
+
+    test('user can not do login action with wrong email or password', function () {
+        $payload = [
+            'email' => 'john@example.com',
+            'password' => 'password',
+        ];
+
+        createUser($payload);
+
+        $this
+            ->post(route('user.authentication.login.store'), [
+                ...$payload,
+                'password' => 'wrong-password',
+            ])
+            ->assertSessionHasErrors([
+                'email'
+            ])
+            ->assertRedirectBack();
+    });
+
+
+    test('user can do login action', function () {
+        $payload = [
+            'email' => 'john@example.com',
+            'password' => 'password',
+        ];
+
+        createUser($payload);
+
+        $this
+            ->post(route('user.authentication.login.store'), $payload)
+            ->assertSessionHasNoErrors()
+            ->assertRedirect(route('user.dashboard.index'));
     });
 });
