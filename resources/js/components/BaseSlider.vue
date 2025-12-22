574e607859b3747bbd06846e8d6e486a75adf572
Nima Asaadi <ni4.asadi.b@gmail.com>
2025-12-22 11:02:14 +0330

    feat: enhance Frequently Asked Questions item with improved button cursor and styling

--- resources/js/components/BaseSlider.vue
+++ resources/js/components/BaseSlider.vue
@@ -8,21 +8,24 @@ defineProps<{
 </script>
 
 <template>
-    <div class="flex flex-col gap-8">
-        <div class="container flex items-center justify-between">
+    <div class="container flex flex-col gap-6">
+        <div class="flex items-center justify-between">
             <h6 class="text-xl">{{ title }}</h6>
             <div v-if="$slots.action">
                 <slot name="action"></slot>
             </div>
         </div>
-        <Swiper
-            slidesPerView="auto"
-            :freeMode="true"
-            :pagination="{ clickable: true }"
-            wrapper-class="[&>*:first-child]:ps-[var(--container-gap)] [&>*:last-child]:pe-[var(--container-gap)]"
-        >
-            <slot name="slides"></slot>
-        </Swiper>
+        <div class="relative">
+            <div class="absolute inset-y-0 start-0 w-50 bg-gradient-to-r from-background to-transparent z-10"></div>
+            <Swiper
+                slidesPerView="auto"
+                :freeMode="true"
+                :pagination="{ clickable: true }"
+            >
+                <slot name="slides"></slot>
+            </Swiper>
+            <div class="absolute inset-y-0 end-0 w-50 bg-gradient-to-l from-background to-transparent z-10"></div>
+        </div>
     </div>
 </template>
 
